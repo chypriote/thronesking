e084901b5dfaa4e1e7d522a720ef01968066b092
NicolasT <nicolastem@hotmail.fr>
2021-03-12 10:31:26 +0100

    reactive prop

--- plugins/chartjs.ts
+++ plugins/chartjs.ts
@@ -4,6 +4,7 @@ import { Line, mixins } from 'vue-chartjs'
 Vue.component('LineChart', {
 	extends: Line,
 	props: ['options'],
+	mixins: [mixins.reactiveProp],
 	mounted () {
 		// @ts-ignore
 		this.renderChart(this.chartData, this.options)
