360a9121ace444b7b61137b6b27bde9092d599ef
NicolasT <nicolastem@hotmail.fr>
2021-07-06 12:27:25 +0200

    add new servers

--- pages/index.vue
+++ pages/index.vue
@@ -13,28 +13,8 @@
 				<fieldset class="field">
 					<div class="select">
 						<label for="server">Select a server</label>
-<!--						<server-select id="server" :servers="servers" @select="setServers" />-->
-						<select id="server" v-model="server">
-							<option value="699" selected>699</option>
-							<option value="532">532</option>
-							<option value="619">619</option>
-							<option value="637">637</option>
-							<option value="653">653</option>
-							<option value="676">676</option>
-							<option value="690">690</option>
-							<option value="691">691</option>
-							<option value="693">693</option>
-							<option value="694">694</option>
-							<option value="695">695</option>
-							<option value="698">698</option>
-							<option value="692">692</option>
-							<option value="696">696</option>
-							<option value="697">697</option>
-							<option value="701">701</option>
-							<option value="707">707</option>
-							<option value="709">709</option>
-							<option value="736">736</option>
-
+						<select id="server" v-model="server" aria-label="Server">
+							<option v-for="item of servs" :key="item" :value="item" selected>{{ item }}</option>
 						</select>
 					</div>
 				</fieldset>
@@ -54,11 +34,13 @@ import Vue from 'vue'
 import TopKingdom from '~/components/Leaderboards/TopKingdom.vue'
 import TopAlliance from '~/components/Leaderboards/TopAlliance.vue'
 import PlayerSelect from '~/components/Global/PlayerSelect.vue'
+import { servs } from '~/store'
 // import ServerSelect from '~/components/Global/ServerSelect.vue'
 
 export default Vue.extend({
 	name: 'Index',
 	components: { PlayerSelect, TopKingdom, TopAlliance },
+	data: () => ({ servs }),
 	computed: {
 		server: {
 			get (): number { return this.$store.state.server },
